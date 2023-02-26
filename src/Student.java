d9dc57040eebb2157a54d4c577526590261f8ce0
Shrirang <sbbhalerao.it@gmail.com>
2023-02-26 18:16:23 +0530

    getters and setters

---
+++ src/Student.java
@@ -0,0 +1,38 @@
+public class Student {
+
+    private String name ;
+    private String city ;
+
+    private String Branch;
+
+
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getBranch() {
+        return Branch;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setBranch(String branch) {
+        Branch = branch;
+    }
+
+
+
+
+}
